c04beee79009b81ba6124c38f7feaf91afdae041
Gago <g.ognjanovski@hotmail.com>
2017-01-14 14:49:40 +0100

    Add Error If Username Not Supplied Functionality

--- cUrl/cUrlCrawler.php
+++ cUrl/cUrlCrawler.php
@@ -5,11 +5,41 @@
  * Date: 1/14/2017
  * Time: 1:54 PM
  */
+require('lib/simple_html_dom.php');
 
-    if(!$_POST["username"]){
-        return [];
-    }
+// if you are doing ajax with application-json headers
+if (empty($_POST)) {
+    return json_response("Input not valid", 400);
+}
 
+
+
+function json_response($message = null, $code = 200)
+{
+    // clear the old headers
+    header_remove();
+    // set the actual code
+    http_response_code($code);
+    // set the header to make sure cache is forced
+    header("Cache-Control: no-transform,public,max-age=300,s-maxage=900");
+    // treat this as json
+    header('Content-Type: application/json');
+    $status = array(
+        200 => '200 OK',
+        400 => '400 Bad Request',
+        422 => 'Unprocessable Entity',
+        500 => '500 Internal Server Error'
+    );
+    // ok, validation error, or failure
+    header('Status: '.$status[$code]);
+    // return the encoded json
+    return json_encode(array(
+        'status' => $code < 300, // success or not?
+        'message' => $message
+    ));
+}
+
+function curlApiWrapper(){
     $ch = curl_init();
 
     $url = 'https://twitter.com/';
@@ -29,8 +59,7 @@
     $scraped_page = curl_exec($ch);
     curl_close($ch);
     echo $scraped_page;
-
-
+}
 
 
 ?>
