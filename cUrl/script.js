82135297239f62321c7160f73607afda51d617cb
viktorsamokov <viktor_veljanoski@hotmail.com>
2017-01-14 15:47:24 +0100

    Create html from ajax call

--- cUrl/script.js
+++ cUrl/script.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
-	console.log("ready");
+	var gigName = $("#gigName");
+	var totalNumberOrders = $("#totalNumberOrders");
+	var gigInfo = $("#gigInfo");
+	var list = '<li class="list-group-item list-group-item-info">';
+	var listEnd = '</li>';
+
+	$.ajax({
+		type: "POST",
+		url: "cUrlCrawler.php",
+		cache: false,
+		success: function(result){
+
+			var html = "";
+			for (var i = 0; i < result.message.PageDetails.length; i++) {
+				var detail = result.message.PageDetails[i];
+				var htmlNode = list + "Title: " + detail.title + " <br /> " + "Number of orders: " + detail.numQueueOrders + listEnd;
+				html += htmlNode;
+			}
+
+			gigName.text(result.username)
+			totalNumberOrders.text(result.message.totalQueueOrders);
+			gigInfo.html(html);
+		}
+	});
 });
